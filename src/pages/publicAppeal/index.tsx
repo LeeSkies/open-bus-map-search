e6da90d36a7d0a73863765bda3c61a3e71b2c7eb
LeeSkies <lee2dskies@gmail.com>
2026-07-15 03:53:30 +0300

    refactor: migrate public appeal page to MUI

--- src/pages/publicAppeal/index.tsx
+++ src/pages/publicAppeal/index.tsx
@@ -1,57 +1,51 @@
-import { Space, Typography } from 'antd'
+import { Box, Link, Stack, Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
-import styled from 'styled-components'
 import Widget from 'src/shared/Widget'
-import './PublicAppeal.scss'
 
-const { Title } = Typography
 const pageName = 'publicAppealPage'
 // product name, not translatable
 const strideApiLinkText = 'Open Bus Stride API'
-const PublicAppeal = () => {
-  const { t } = useTranslation()
-  const tasks = t(`${pageName}.tasks`, { returnObjects: true })
-
-  return (
-    <PublicAppealStyle>
-      <Space direction="vertical" size="middle">
-        <Title className="page-title" level={3}>
-          {t(`${pageName}.title`)}
-        </Title>
-        {tasks.map((task, i) => (
-          <Task {...task} marginBottom={tasks.length - 1 === i} key={i} />
-        ))}
-      </Space>
-    </PublicAppealStyle>
-  )
-}
 
 type TaskDetails = {
   title: string
   description: string
-  marginBottom?: boolean
 }
 
-const Task = ({ title, description, marginBottom }: TaskDetails) => {
+const PublicAppeal = () => {
+  const { t } = useTranslation()
+  const tasks = t(`${pageName}.tasks`, { returnObjects: true }) as TaskDetails[]
+
   return (
-    <Widget title={title} marginBottom={marginBottom}>
-      <p>{description}</p>
-      <a href="https://open-bus-stride-api.hasadna.org.il/docs">{strideApiLinkText}</a>
-    </Widget>
+    <Box component="main" sx={{ px: 2, width: '100%' }}>
+      <Stack spacing={2} sx={{ mx: 'auto', width: '100%', maxWidth: 960 }}>
+        <Typography
+          component="h1"
+          variant="h4"
+          className="page-title"
+          sx={{ fontWeight: 700, textWrap: 'balance' }}>
+          {t(`${pageName}.title`)}
+        </Typography>
+        {tasks.map((task) => (
+          <Task {...task} key={task.title} />
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
-const PublicAppealStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 0 1rem;
-  & .public-appeal-center-container {
-    width: 100%;
-    max-width: 770px;
-    & h1 {
-      font-size: 2em;
-    }
-  }
-`
+const Task = ({ title, description }: TaskDetails) => {
+  return (
+    <Widget title={title}>
+      <Typography component="p" sx={{ textWrap: 'pretty' }}>
+        {description}
+      </Typography>
+      <Link
+        href="https://open-bus-stride-api.hasadna.org.il/docs"
+        sx={{ display: 'inline-flex', alignItems: 'center', minHeight: 40 }}>
+        {strideApiLinkText}
+      </Link>
+    </Widget>
+  )
+}
 
 export default PublicAppeal
